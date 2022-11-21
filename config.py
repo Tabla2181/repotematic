51fdb83078b3615a853a6f2a3067dcc6138add77
Abolanosupload <111134992+Abolanosupload@users.noreply.github.com>
2022-11-21 17:51:09 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID =  9902519 #Tu api id de telegram
 API_HASH = '9d8097d05bbc90a6ed2a7a81abcd4e8a' #Tu api id de telegram
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','Abolanos3',').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','Abolanos3').split(';')
 
 static_proxy = '' #agrega si kieres tener un proxy statico Con @raydel0307 si kieres comprar un proxy
 PROXY = ProxyCloud.parse(static_proxy)

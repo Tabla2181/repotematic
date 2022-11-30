ebff28e8d17f009e11b9f1f86b4b081106aedd0b
Abolanosupload <111134992+Abolanosupload@users.noreply.github.com>
2022-11-30 12:23:53 -0500

    Update downloader.py

--- pydownloader/downloader.py
+++ pydownloader/downloader.py
@@ -26,15 +26,8 @@ class Downloader(object):
         self.progressfunc = progressfunc
         self.args = args
         req = None
-        if 'youtube' in url or 'youtu.be' in url:
-                try:
-                    data = await youtube.getVideoData(url)
-                    if data:
-                        url = data['url']
-                        self.filename = slugify(data['name'])
-                    else: return None
-                except: return None
-        elif 'mediafire' in url:
+
+        if 'mediafire' in url:
                 try:
                     url = mediafire.get(url)
                 except:return None

5d30cd0ce5f445ceb04b010273f7188cf3a8dc82
Tabla2181 <102341979+Tabla2181@users.noreply.github.com>
2022-11-30 13:58:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ from pydownloader.downloader import Downloader
 import shorturl
 import xdlink
 
-tl_admin_users = ['Abolanos3',] #Poner aqui los user con acceso permanente
-godlist = ['Abolanos3'] #Poner aqui los admin 
+tl_admin_users = ['Elnietodecacha',] #Poner aqui los user con acceso permanente
+godlist = ['Elnietodecacha'] #Poner aqui los admin 
 
 async def get_root(username):
     if os.path.isdir(config.ROOT_PATH+username)==False:
